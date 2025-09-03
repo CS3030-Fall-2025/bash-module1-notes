3e7f8cdca1093c60fa6001a84906b071687b9c05
Drew Weidman <dweidman@weber.edu>
2025-09-03 07:04:18 -0700

    Update task3.sh

--- notes/task3.sh
+++ notes/task3.sh
@@ -1,32 +1,15 @@
 #! /usr/bin/bash
+
+# Promt user for name and store value entered
 printf "Enter your name:\n"
-read name
 
-if [[ -z $name ]]
-then
-    # >&2 for stderr
-    echo "No name entered" >&2
-    exit 1
+# Check to see if no name is entered
 
-fi 
-
-printf "Hello %s\n" $name
-printf "Let's play a game."
+# Greet user by their name and say Let's Play a game
 
 # Ask for two numbers neither of which is less than 10 or greater than 20
-printf "Enter a number between 10 and 20: "
-read number # captures the input
-# two bash comands have to be separated by a semicolon ;
-if (( number > 20 ))
-then
-    # integers in printf use %d
-    printf "the number %d is too high\n" "$number" >&2
-    exit 1
-elif (( number < 10 )) ; then
-    printf "The number %d is too low\n" "$number" >&2
-    exit 1
-else
-    printf "You entered %d as a number\n" "$number"
-fi 
 
-exit 0
+# two bash comands have to be separated by a semicolon ;
+
+    # integers in printf use %d
+ 
