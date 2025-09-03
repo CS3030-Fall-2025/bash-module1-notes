f1be0e4a080bff7f0d841cb39c79a25b508d37c5
Drew Weidman <dweidman@weber.edu>
2025-09-03 06:48:22 -0700

    Update tables.sh

--- notes/tables.sh
+++ notes/tables.sh
@@ -1,14 +1,3 @@
 #! /usr/bin/bash
 
 # Print multiplication tables 1 -> 12
-for (( i=1; i<=12; i++ ))
-do
-    for (( j=1; j<=12; j++ ))
-    do 
-        num=$(( i*j ))
-        echo -n "$num " $'\t'
-    done
-    echo
-done
-
-exit 0
