57c742563edc58c1788cd8e888cf5a5e53438b97
Drew Weidman <dweidman@weber.edu>
2025-09-03 07:00:08 -0700

    Update task1.sh

--- notes/task1.sh
+++ notes/task1.sh
@@ -2,22 +2,12 @@
 
 #Write a script that creates a directory called bpl inside of $HOME.
 #Populate this directory with two subdirectories called: bin and scripts
-printf "Creating directory bpl inside $HOME\n"
-cd $HOME  # change to the directory of $HOME
-mkdir bpl  # create the bpl directory
-cd bpl # change to the directory bpl
-printf "Creating subdirectories of bin and scripts\n"
-mkdir bin scripts # create the two subdirectories of bin and scripts
-ls -l
-
+  # change to the directory of $HOME
+ # create the bpl directory
+# change to the directory bpl
+ # create the two subdirectories of bin and scripts
+ # show subdirectories
 
 # Remove the subdirectories we created
-printf "Deleting subdirectories\n"
-rmdir bin scripts
-cd ..
-printf "Delete the bpl directory inside of $HOME\n"
-rmdir bpl
-ls -l
-
-
-exit 0
+# move up a directory
+# show directories
