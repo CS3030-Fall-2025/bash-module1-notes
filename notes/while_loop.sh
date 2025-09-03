51c033a485a6eaad08a38d21f160fcd8647ad2c3
Drew Weidman <dweidman@weber.edu>
2025-09-03 07:08:38 -0700

    Update while_loop.sh

--- notes/while_loop.sh
+++ notes/while_loop.sh
@@ -1,14 +1,10 @@
 #! /usr/bin/bash
-num=1
-clue=5
+# set number and clue values
+
 #Test while loop
 #Syntax: While <condition>
 # do
 #   while ($num < $clue) {print it; $num++}
-while [[ $num -lt $clue ]] ; do 
-    echo $num
     # when variables are number, we will use (()) instead of [[]]
-    num=$(( num+1 ))
-done
-
-exit 0
+    # increment number
+ 
