c926632b9e7623ce117a97f5529c6933199c0041
Drew Weidman <dweidman@weber.edu>
2025-09-03 06:41:14 -0700

    Update input_name.sh

--- notes/input_name.sh
+++ notes/input_name.sh
@@ -1,16 +1,8 @@
 #! /usr/bin/bash
 
 # Read and check input
-echo "What is your name?"
-# read user input
-read name
-# Test if variable is not "null"
-if [[ -z "$name" ]]
-then
-    echo "No name was entered"
-    exit 1 # set failed code
-else
-    echo "Hello $name"
-fi 
 
-exit 0
+# read user input
+
+# Test if variable is not "null"
+
