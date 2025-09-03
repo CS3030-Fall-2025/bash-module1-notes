f19bfb5d72615e5a33d74b10dce9ab84146edcca
Drew Weidman <dweidman@weber.edu>
2025-09-03 07:07:25 -0700

    Update task5.sh

--- notes/task5.sh
+++ notes/task5.sh
@@ -1,23 +1,11 @@
 #! /usr/bin/bash
+# set guess number
 
-guess=1
+# check that guesses is less than 3
 
-while [[ $guess -le 3 ]]
-do
 #    printf "Enter the file name you are looking for in %s\n" "$PWD"
-    printf "Enter the file name you are looking for in $PWD\n"
-    read infile 
+
     # test if file exists
-    if [[ -e $infile ]]
-    then
-        printf "Found %s file in this folder\n" "$infile"
-        break
-    else
-        printf "Could not find %s in this folder\n" "$infile"
-    fi
+ 
     #update guess
-    guess=$(( $guess + 1 ))
-
-done
-
-echo "Done"
+    
