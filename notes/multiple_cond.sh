48ed0f8b76f0ea8ca47483fde20dd5398e022e64
Drew Weidman <dweidman@weber.edu>
2025-09-03 06:44:07 -0700

    Update multiple_cond.sh

--- notes/multiple_cond.sh
+++ notes/multiple_cond.sh
@@ -1,16 +1,13 @@
 #! /usr/bin/bash
 
 # Ask for two numbers neither of which is less than 10 or greater than 20
-printf "Enter a number between 10 and 20: "
-read number # captures the input
-# two bash comands have to be separated by a semicolon ;
-if [[ "$number" -gt 20 ]]; then
-    # integers in printf use %d
-    printf "the number %d is too high\n" "$number"
-elif [[ "$number" -lt 10 ]] ; then
-    printf "The number %d is too low\n" "$number"
-else
-    printf "You entered %d as a number\n" "$number"
-fi 
 
-exit 0
+# two bash comands have to be separated by a semicolon ;
+# check if number greater than 20
+
+    # integers in printf use %d
+
+# check if number less than 10
+
+# show the number
+
