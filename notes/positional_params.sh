fd219fdb33704146f8f031a3d447c3db9c27b8c1
Drew Weidman <dweidman@weber.edu>
2025-09-03 06:47:58 -0700

    Update positional_params.sh

--- notes/positional_params.sh
+++ notes/positional_params.sh
@@ -1,43 +1,21 @@
 #! /usr/bin/bash
 #Print all postional Parameters $@
+# print name of program
 
-echo "You are running with $@"
 
 # Print total number of parameters $#
 #If it does not count the program itself ($0)
-echo "$0 fname lname"
 
-if [[ $# -eq 2 ]]
-then 
-    echo "You are good to go"
-    fname=$1
-    lname=$2
-else
-    echo "Enter your first name: "
-    read fname
-    if [[ -z "$fname" ]]
-    then
-        echo "No first name was entered"
-        exit 1
-    fi
-    echo "Enter your last name: "
-    read lname
-    
-    if [[ -z "$lname" ]]
-    then
-        echo "No last name was entered"
-        exit 1
-    fi
+# if number equals correct value print message about being able to proceed
+
+# prompt for the first name and check if it is blank
+
+  # prompt for the last name and check if it is blank
 
  #   echo "Good Bye. You have the incorrect number of parameters"
  #   echo "You have this number of parameters: $#"
  #   echo "Usage: $0 fname lname"
  #   exit 1
 
-fi
-
 
 # Print values with $
-echo "Hi $fname $lname"
-
-exit 0
