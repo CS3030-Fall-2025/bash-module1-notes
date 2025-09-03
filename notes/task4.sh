fc068888151eaa5616cca696d8dba16fed481864
Drew Weidman <dweidman@weber.edu>
2025-09-03 07:06:01 -0700

    Update task4.sh

--- notes/task4.sh
+++ notes/task4.sh
@@ -1,16 +1,9 @@
 #! /usr/bin/bash
+# Set dir to HOME/bin
 
-dir=$HOME/bin
+
+# Check if directory already exists
+# If it exists, move into it and print out message that it exists
 # -d of ls is for directory
-if [[ -d "$dir" ]] && cd "$dir"
-then
-    echo "Directory exists"
-    echo "Here I am $PWD"
-else
-# if directory does not exist, create the directory and move to it
-    echo "Creating $dir"
-    mkdir $dir && cd $dir
-    echo "Here I am $PWD"
-fi
 
-exit 0
+# if directory does not exist, create the directory and move to it
